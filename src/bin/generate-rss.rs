bb39e9aece8a8514f97c9835b3f5f4bdd5d91073
Wesley Moore <wes@wezm.net>
2018-01-21 13:33:02 +1100

    Implement some of the checks in script/check in generate-rss

--- src/bin/generate-rss.rs
+++ src/bin/generate-rss.rs
@@ -3,6 +3,7 @@ extern crate rss;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate chrono;
 
 use std::env;
 use std::fs::File;
@@ -10,7 +11,9 @@ use std::path::Path;
 
 use rss::{ChannelBuilder, ItemBuilder, GuidBuilder};
 
-use read_rust::feed::{Feed};
+use chrono::{DateTime, FixedOffset, Datelike};
+
+use read_rust::feed::{Feed, Item};
 use read_rust::error::Error;
 
 const ARG_COUNT: usize = 3;
@@ -22,10 +25,52 @@ struct Post<'a> {
     author_name: &'a str,
 }
 
-fn generate_rss_items(feed: &Feed) -> Result<Vec<rss::Item>, Error> {
-    let mut items = Vec::with_capacity(feed.items.len());
+// Need TryFrom/Into https://github.com/sfackler/rfcs/blob/try-from/text/0000-try-from.md
+pub trait TryFrom<T>: Sized {
+    type Err;
 
-    for item in feed.items.iter() {
+    fn try_from(t: T) -> Result<Self, Self::Err>;
+}
+
+pub trait TryInto<T>: Sized {
+    type Err;
+
+    fn try_into(self) -> Result<T, Self::Err>;
+}
+
+impl<T, U> TryInto<U> for T where U: TryFrom<T> {
+    type Err = U::Err;
+
+    fn try_into(self) -> Result<U, Self::Err> {
+        U::try_from(self)
+    }
+}
+
+fn unwrap_placeholder(text: &str) -> Result<String, Error> {
+    if text.chars().all(char::is_whitespace) {
+        Err(Error::StringError("value is empty string".to_owned()))
+    }
+    else if text == "FIXME" {
+        Err(Error::StringError("value is FIXME".to_owned()))
+    }
+    else {
+        Ok(text.to_owned())
+    }
+}
+
+fn unwrap_date(date: &DateTime<FixedOffset>) -> Result<&DateTime<FixedOffset>, Error> {
+    if date.year() < 2018 {
+        Err(Error::StringError("date is before 2018".to_owned()))
+    }
+    else {
+        Ok(date)
+    }
+}
+
+impl TryFrom<Item> for rss::Item {
+    type Err = Error;
+
+    fn try_from(item: Item) -> Result<Self, Self::Err> {
         let guid = GuidBuilder::default()
             .value(item.id.to_string())
             .permalink(false)
@@ -33,20 +78,30 @@ fn generate_rss_items(feed: &Feed) -> Result<Vec<rss::Item>, Error> {
             .map_err(|err| Error::StringError(err))?;
 
         let dc_extension = rss::extension::dublincore::DublinCoreExtensionBuilder::default()
-            .creators(vec![item.author.name.clone()])
+            .creators(vec![unwrap_placeholder(&item.author.name)?])
             .build()
             .map_err(|err| Error::StringError(err))?;
 
-        let item = ItemBuilder::default()
+        // The author URL isn't used but verify that it's not a placeholder anyway
+        let _ = unwrap_placeholder(&item.author.url)?;
+
+        ItemBuilder::default()
             .guid(Some(guid))
-            .title(item.title.clone())
+            .title(unwrap_placeholder(&item.title)?)
             .link(item.url.clone())
-            .description(item.content_text.clone())
-            .pub_date(item.date_published.to_rfc2822())
+            .description(unwrap_placeholder(&item.content_text)?)
+            .pub_date(unwrap_date(&item.date_published)?.to_rfc2822())
             .dublin_core_ext(dc_extension)
             .build()
-            .map_err(|err| Error::StringError(err))?;
-        items.push(item);
+            .map_err(|err| Error::StringError(err))
+    }
+}
+
+fn generate_rss_items(feed: &Feed) -> Result<Vec<rss::Item>, Error> {
+    let mut items = Vec::with_capacity(feed.items.len());
+
+    for item in feed.items.iter() {
+        items.push(item.clone().try_into()?);
     }
 
     Ok(items)
@@ -101,4 +156,20 @@ fn main() {
     }
 
     run(&args[0], &args[1], &args[2]).expect("error!");
+}
+
+#[test]
+fn test_unwrap_placeholder() {
+    assert!(unwrap_placeholder("").is_err());
+    assert!(unwrap_placeholder("   ").is_err());
+    assert!(unwrap_placeholder("FIXME").is_err());
+    assert!(unwrap_placeholder("Ok").is_ok());
+}
+
+#[test]
+fn test_unwrap_date() {
+    use chrono::TimeZone;
+
+    assert!(unwrap_date(&FixedOffset::east(0).ymd(1970, 1, 1).and_hms(0, 0, 0)).is_err());
+    assert!(unwrap_date(&FixedOffset::east(0).ymd(2018, 1, 1).and_hms(0, 0, 0)).is_ok());
 }
