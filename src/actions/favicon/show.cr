adbe5bb279022343807cf1f4ba830c404f9cbb30
Wesley Moore <wes@wezm.net>
2019-10-03 17:03:24 +0800

    Move favicon out of assets

--- src/actions/favicon/show.cr
+++ src/actions/favicon/show.cr
@@ -2,6 +2,6 @@ class Favicon::Show < BrowserAction
   include Auth::AllowGuests
 
   get "/favicon.ico" do
-    file "public/assets/favicon.ico", disposition: "inline"
+    file "public/favicon.ico", disposition: "inline"
   end
 end
