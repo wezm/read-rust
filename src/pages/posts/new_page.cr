5994b09864ddaab63b57ee8d0c530418160332ff
Wesley Moore <wes@wezm.net>
2019-10-20 10:32:02 +1100

    Mark form fields as required

--- src/pages/posts/new_page.cr
+++ src/pages/posts/new_page.cr
@@ -9,12 +9,12 @@ class Posts::NewPage < AdminLayout
 
   def render_post_form(f)
     form_for Posts::Create, id: "new-post-form", class: "form-stacked" do
-      mount Shared::Field.new(f.title), &.text_input(autofocus: "true")
-      mount Shared::Field.new(f.author)
-      mount Shared::Field.new(f.url, "URL"), &.url_input
+      mount Shared::Field.new(f.title), &.text_input(attrs: [:autofocus, :required])
+      mount Shared::Field.new(f.author), &.text_input(attrs: [:required])
+      mount Shared::Field.new(f.url, "URL"), &.url_input(attrs: [:required])
       mount Shared::Field.new(f.twitter_url, "Twitter URL"), &.url_input
       mount Shared::Field.new(f.mastodon_url, "Fediverse URL"), &.url_input
-      mount Shared::Field.new(f.summary), &.textarea
+      mount Shared::Field.new(f.summary), &.textarea(required: "required")
 
       fieldset(class: "categories") do
         tag "legend" { text "Categories" }
