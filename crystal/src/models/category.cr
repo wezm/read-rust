de0d7b6c7cc56dffdf2b7fa6436483a1f74503ba
Wesley Moore <wes@wezm.net>
2019-10-28 10:08:17 +1100

    Load categories from JSON in Crystal code

--- crystal/src/models/category.cr
+++ crystal/src/models/category.cr
@@ -1,31 +1,18 @@
 class Category
-  getter id, name, hashtag, slug, description
+  JSON.mapping(
+    id: Int16,
+    name: String,
+    hashtag: String,
+    slug: String,
+    description: String,
+  )
 
-  ALL = [
-    Category.new(0,  "All",                      "all",         "all",                      "All posts."),
-    Category.new(1,  "Community",                "community",   "community",                "Initiatives in the Rust community."),
-    Category.new(2,  "Computer Science",         "compsci",     "computer-science",         "Covering data structures, algorithms, memory safety, etc."),
-    Category.new(3,  "Crates",                   "crates",      "crates",                   "Notable new or updated crates."),
-    Category.new(4,  "DevOps and Deployment",    "devops",      "devops-and-deployment",    "Building and deploying Rust, containerisation, continuous integration, etc."),
-    Category.new(5,  "Embedded",                 "embedded",    "embedded",                 "Rust on microcontrollers, IoT, devices."),
-    Category.new(6,  "Games and Graphics",       "graphics",    "games-and-graphics",       "Games built with Rust and other graphics related work."),
-    Category.new(7,  "Getting Started",          "starting",    "getting-started",          "Introductory posts, guides and tutorials for getting started with Rust."),
-    Category.new(8,  "Language",                 "language",    "language",                 "General posts about the Rust language."),
-    Category.new(9,  "Operating Systems",        "os",          "operating-systems",        "Using Rust to build or explore operating systems."),
-    Category.new(10, "Performance",              "performance", "performance",              "Optimisation, benchmarks, etc."),
-    Category.new(11, "Rust 2018",                "Rust2018",    "rust-2018",                "Hopes and dreams for Rust in 2018."),
-    Category.new(12, "Rust 2019",                "rust2019",    "rust-2019",                "Ideas from the community for Rust in 2019, and the next edition."),
-    Category.new(13, "Tools and Applications",   "tools",       "tools-and-applications",   "Command line tools and GUI applications."),
-    Category.new(14, "Security",                 "security",    "security",                 "Security, cryptography, etc."),
-    Category.new(15, "Web and Network Services", "web",         "web-and-network-services", "Web applications, web assembly, network daemons, etc."),
-  ]
+  ALL = Array(Category).from_json(File.read(File.join(__DIR__, "..", "..", "..", "content", "_data", "categories.json")))
+  VALID_IDS = ALL.compact_map { |category| category.all? ? nil : category.id }
 
   # Return the list of category ids that are valid for a PostCategory record
   def self.valid_ids
-    ALL.compact_map { |category| category.all? ? nil : category.id }
-  end
-
-  def initialize(@id : Int16, @name : String, @hashtag : String, @slug : String, @description : String)
+    VALID_IDS
   end
 
   def all?
