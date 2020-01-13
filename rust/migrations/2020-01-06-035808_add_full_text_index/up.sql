4ba2da5357e9a06d08cee9c5f9a44f1364854b07
Wesley Moore <wes@wezm.net>
2020-01-13 18:47:36 +1100

    Fix indexing of tags

--- rust/migrations/2020-01-06-035808_add_full_text_index/up.sql
+++ rust/migrations/2020-01-06-035808_add_full_text_index/up.sql
@@ -2,12 +2,12 @@ CREATE MATERIALIZED VIEW search_view AS
     SELECT posts.id,
            posts.summary,
            setweight(to_tsvector('english', posts.title), 'A') ||
-               setweight(to_tsvector('english', string_agg(' ', tags.name)), 'B') ||
+               setweight(to_tsvector('english', coalesce(string_agg(tags.name, ' '), '')), 'B') ||
                setweight(to_tsvector('english', posts.summary), 'C') ||
                setweight(to_tsvector('english', posts.author), 'D') AS vector
     FROM posts
-    LEFT JOIN post_tags ON (post_tags.post_id = posts.id)
-    LEFT JOIN tags ON (tags.id = post_tags.tag_id)
+    LEFT JOIN post_tags ON (posts.id = post_tags.post_id)
+    LEFT JOIN tags ON (post_tags.tag_id = tags.id)
     GROUP BY posts.id;
 
 CREATE INDEX search_index ON search_view USING GIN (vector);
