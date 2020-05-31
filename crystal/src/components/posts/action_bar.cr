bd1e0157ab05516a1038e72e2449f857d60e1337
Wesley Moore <wes@wezm.net>
2020-05-31 13:59:43 +1000

    Show tweet/toot links in action bar

--- crystal/src/components/posts/action_bar.cr
+++ crystal/src/components/posts/action_bar.cr
@@ -7,6 +7,10 @@ class Posts::ActionBar < BaseComponent
       div class: "post-action-bar" do
         link "Show", Posts::Show.with(@post.id)
         link "Edit", Posts::Edit.with(@post.id)
+        twitter_url = @post.twitter_url
+        mastodon_url = @post.mastodon_url
+        a(href: twitter_url) { text "Tweet URL" } if twitter_url
+        a(href: mastodon_url) { text "Fediverse URL" } if mastodon_url
         # link "Delete", Posts::Delete.with(@post.id), data_confirm: "Are you sure?"
       end
     end
